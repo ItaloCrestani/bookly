75dca56db13d474975f2cca2c2a976b29c87ccce
Italo Crestani <italocrestani09@gmail.com>
2026-06-26 22:20:48 -0300

    update images http to https

--- src/data/featuredBooks.ts
+++ src/data/featuredBooks.ts
@@ -53,70 +53,70 @@
 export const featuredBooks = [
     {
         id: "PM2uCgAAQBAJ",
-        image: "http://books.google.com/books/content?id=PM2uCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=PM2uCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Messias de Duna",
         author: "Frank Herbert",
         genre: "Fiction",
     },
     {
         id: "c706EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=c706EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=c706EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "A revolução dos bichos",
         author: "George Orwell",
         genre: "Young Adult Fiction",
     },
     {
         id: "VPITEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=VPITEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=VPITEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "1984",
         author: "George Orwell",
         genre: "Fiction",
     },
     {
         id: "qDYQCwAAQBAJ",
-        image: "http://books.google.com/books/content?id=qDYQCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=qDYQCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Harry Potter e o prisioneiro de Azkaban",
         author: "J. K. Rowling",
         genre: "Juvenile Fiction",
     },
     {
         id: "FP-rXfdk7EoC",
-        image: "http://books.google.com/books/content?id=FP-rXfdk7EoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=FP-rXfdk7EoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O ladrão de raios",
         author: "Rick Riordan",
         genre: "Young Adult Fiction",
     },
     {
         id: "fWd6EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=fWd6EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=fWd6EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O Senhor dos Anéis",
         author: "J.R.R. Tolkien",
         genre: "Fiction",
     },
     {
         id: "2LeZDwAAQBAJ",
-        image: "http://books.google.com/books/content?id=2LeZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=2LeZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O Hobbit",
         author: "J.R.R. Tolkien",
         genre: "Fiction",
     },
     {
         id: "Hw_MDgAAQBAJ",
-        image: "http://books.google.com/books/content?id=Hw_MDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=Hw_MDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Box - Em busca do tempo perdido",
         author: "Marcel Proust",
         genre: "Fiction",
     },
     {
         id: "7-9zwIWkAlEC",
-        image: "http://books.google.com/books/content?id=7-9zwIWkAlEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=7-9zwIWkAlEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O nome do vento",
         author: "Patrick Rothfuss",
         genre: "Fiction",
     },
     {
         id: "MTtpDwAAQBAJ",
-        image: "http://books.google.com/books/content?id=MTtpDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=MTtpDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O príncipe cruel (Vol. 1 O povo do ar)",
         author: "Holly Black",
         genre: "Fiction",
@@ -126,70 +126,70 @@ export const featuredBooks = [
 export const classicBooks = [
     {
         id: "LPKyUG4OmlsC",
-        image: "http://books.google.com/books/content?id=LPKyUG4OmlsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=LPKyUG4OmlsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Odisseia",
         author: "Homero",
         genre: "Fiction"
     },
     {
         id: "GFvRDwAAQBAJ",
-        image: "http://books.google.com/books/content?id=GFvRDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=GFvRDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "A tragédia de Hamlet, príncipe da Dinamarca",
         author: "William Shakespeare",
         genre: "Art"
     },
     {
         id: "2z83EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=2z83EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=2z83EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "A canção de Aquiles",
         author: "Madeline Miller",
         genre: "Fiction"
     },
     {
         id: "jycGEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=jycGEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=jycGEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Dom Quixote",
         author: "Miguel de Cervantes Saavedra",
         genre: "Young Adult Fiction"
     },
     {
         id: "OXS9EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=OXS9EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=OXS9EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Orgulho e preconceito",
         author: "Jane Austen",
         genre: "Fiction"
     },
     {
         id: "tBz8DwAAQBAJ",
-        image: "http://books.google.com/books/content?id=tBz8DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=tBz8DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Crime e castigo",
         author: "Fiódor Dostoiévski",
         genre: "Fiction"
     },
     {
         id: "Eg0zEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=Eg0zEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=Eg0zEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O morro dos ventos uivantes",
         author: "Emily Brontë",
         genre: "Fiction"
     },
     {
         id: "q0AQEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=q0AQEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=q0AQEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Frankenstein",
         author: "Mary Shelley",
         genre: "Fiction"
     },
     {
         id: "HoApEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=HoApEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=HoApEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Drácula - volume 1 - Coleção Mistério & Suspense",
         author: "Bram Stoker",
         genre: "Fiction"
     },
     {
         id: "q2o0BQAAQBAJ",
-        image: "http://books.google.com/books/content?id=q2o0BQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=q2o0BQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Os miseráveis",
         author: "Victor Hugo",
         genre: "Juvenile Fiction"
@@ -199,70 +199,70 @@ export const classicBooks = [
 export const nationalBooks = [
     {
         id: "qmE0EQAAQBAJ",
-        image: "http://books.google.com/books/content?id=qmE0EQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=qmE0EQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Dom Casmurro",
         author: "Machado de Assis",
         genre: "Fiction"
     },
     {
         id: "p-hJknjfOE0C",
-        image: "http://books.google.com/books/content?id=p-hJknjfOE0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=p-hJknjfOE0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Memórias Póstumas De Brás Cubas",
         author: "Machado de Assis",
         genre: "Fiction"
     },
     {
         id: "vb70EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=vb70EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=vb70EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Vidas Secas - Edição Luxo",
         author: "Graciliano Ramos ",
         genre: "Fiction"
     },
     {
         id: "FDJ1_r4MCIEC",
-        image: "http://books.google.com/books/content?id=FDJ1_r4MCIEC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70peiP-Hk116dBQmwwaNrBQITD4bmndybjR7IL4715MEivRaAOYuy-SLlZ39519dH9CcMi5gVVe2_kTlaQP1UKt4ig1QsAU2zufZXVhQ_EdwC6llIFCgFox4i_tcKP5iM-wx3y0&source=gbs_api",
+        image: "https://books.google.com/books/content?id=FDJ1_r4MCIEC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70peiP-Hk116dBQmwwaNrBQITD4bmndybjR7IL4715MEivRaAOYuy-SLlZ39519dH9CcMi5gVVe2_kTlaQP1UKt4ig1QsAU2zufZXVhQ_EdwC6llIFCgFox4i_tcKP5iM-wx3y0&source=gbs_api",
         title: "Capitães da areia",
         author: "Jorge Amado",
         genre: "Fiction"
     },
     {
         id: "82UHEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=82UHEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=82UHEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "A hora da estrela",
         author: "Clarice Lispector",
         genre: "Fiction"
     },
     {
         id: "s9kiEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=s9kiEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=s9kiEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Grande sertão: veredas",
         author: "João Guimarães Rosa",
         genre: "Fiction"
     },
     {
         id: "Skb9CAAAQBAJ",
-        image: "http://books.google.com/books/publisher/content?id=Skb9CAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70GWXYFXWSAPNFucmosC5nouxWKcMvqqK82UB0aEL37I1X6IyLGi0XKHxKg5B35hPPfQbWznQxWpxh2L270pT_yAmyPmgDqiVQHJcjCZ7MTdPrjh8jIQNNXZnGlwz5wQpERYEFh&source=gbs_api",
+        image: "https://books.google.com/books/publisher/content?id=Skb9CAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70GWXYFXWSAPNFucmosC5nouxWKcMvqqK82UB0aEL37I1X6IyLGi0XKHxKg5B35hPPfQbWznQxWpxh2L270pT_yAmyPmgDqiVQHJcjCZ7MTdPrjh8jIQNNXZnGlwz5wQpERYEFh&source=gbs_api",
         title: "Iracema",
         author: "José de Alencar",
         genre: "Fiction"
     },
     {
         id: "Z0hpEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=Z0hpEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=Z0hpEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O cortiço",
         author: "Aluísio de Azevedo",
         genre: "Fiction"
     },
     {
         id: "jxP3EAAAQBAJ",
-        image: "http://books.google.com/books/content?id=jxP3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=jxP3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Senhora",
         author: "José de Alencar",
         genre: "Fiction"
     },
     {
         id: "7aV-0QEACAAJ",
-        image: "http://books.google.com/books/content?id=7aV-0QEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        image: "https://books.google.com/books/content?id=7aV-0QEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
         title: "Quarto de Despejo - Carolina Maria de Jesus (análise de livro)",
         author: "Tomás Rodrigues",
         genre: "Literary Criticism"
@@ -272,70 +272,70 @@ export const nationalBooks = [
 export const fictionBooks = [
     {
         id: "ZDgQCwAAQBAJ",
-        image: "http://books.google.com/books/content?id=ZDgQCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=ZDgQCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Harry Potter e o Cálice de Fogo",
         author: "J.K. Rowling",
         genre: "Juvenile Fiction"
     },
     {
         id: "FP-rXfdk7EoC",
-        image: "http://books.google.com/books/content?id=FP-rXfdk7EoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=FP-rXfdk7EoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O ladrão de raios",
         author: "Rick Riordan",
         genre: "Young Adult Fiction"
     },
     {
         id: "2LeZDwAAQBAJ",
-        image: "http://books.google.com/books/content?id=2LeZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=2LeZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O Hobbit",
         author: "J.R.R. Tolkien",
         genre: "Fiction"
     },
     {
         id: "o39zEAAAQBAJ",
-        image: "http://books.google.com/books/content?id=o39zEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=o39zEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "A Sociedade do Anel - Capa Os Anéis de Poder",
         author: "J.R.R. Tolkien",
         genre: "Fiction"
     },
     {
         id: "j9KeEQAAQBAJ",
-        image: "http://books.google.com/books/content?id=j9KeEQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=j9KeEQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "As Crônicas de Nárnia – Edição comemorativa de 75 anos – Capa deluxe com jacket",
         author: "C.S. Lewis",
         genre: "Juvenile Fiction"
     },
     {
         id: "Xt9e9yLsyuAC",
-        image: "http://books.google.com/books/content?id=Xt9e9yLsyuAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=Xt9e9yLsyuAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Eragon",
         author: "Christopher Paolini",
         genre: "Young Adult Fiction"
     },
     {
         id: "7-9zwIWkAlEC",
-        image: "http://books.google.com/books/content?id=7-9zwIWkAlEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=7-9zwIWkAlEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O nome do vento",
         author: "Patrick Rothfuss",
         genre: "Fiction"
     },
     {
         id: "KD9pEQAAQBAJ",
-        image: "http://books.google.com/books/content?id=KD9pEQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=KD9pEQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "Mistborn: O Império Final",
         author: "Brandon Sanderson",
         genre: "Fiction"
     },
     {
         id: "ZeGWAAAAQBAJ",
-        image: "http://books.google.com/books/content?id=ZeGWAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=ZeGWAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O olho do mundo",
         author: "Robert Jordan",
         genre: "Fiction"
     },
     {
         id: "RhqJWjZ_6o4C",
-        image: "http://books.google.com/books/content?id=RhqJWjZ_6o4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        image: "https://books.google.com/books/content?id=RhqJWjZ_6o4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         title: "O guia do mochileiro das galáxias",
         author: "Douglas Adams",
         genre: "Fiction"
